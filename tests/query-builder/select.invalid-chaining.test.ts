94b23ad76bb19dd51d6b04267ff8e6e3a5463408
Abel Osorio <abel.m.osorio@gmail.com>
2025-04-04 14:27:51 -0300

    Implemented a first test for invalid chainings.

---
+++ tests/query-builder/select.invalid-chaining.test.ts
@@ -0,0 +1,30 @@
+import QueryBuilder from '~/lib/query-builder'
+
+describe('Invalid chaining', () => {
+  it('should not allow SELECT after WHERE', () => {
+    const qb = new QueryBuilder()
+
+    expect(() => qb.where('a IS TRUE').select()).toThrow()
+  })
+
+  // Exception for subqueries: SELECT * FROM ( SELECT... )
+  it('should not allow SELECT after FROM', () => {
+    const qb = new QueryBuilder()
+
+    expect(() => qb.from('table').select()).toThrow()
+  })
+
+
+  it('should not allow FROM after WHERE', () => {
+    const qb = new QueryBuilder()
+
+    expect(() => qb.where('a IS TRUE').from('table')).toThrow()
+  })
+
+  // Exception for subqueries: WHERE a IN ( SELECT... )
+  it('should not allow SELECT after WHERE', () => {
+    const qb = new QueryBuilder()
+
+    expect(() => qb.where('a IS TRUE').select()).toThrow()
+  })
+})
