41234fd49756afa13589811049f677610a4714c3
Abel Osorio <abel.m.osorio@gmail.com>
2025-04-04 12:53:36 -0300

    Initial draft for the Query builder.

---
+++ tests/query-builder/select.test.js
@@ -0,0 +1,24 @@
+import QueryBuilder from '../../lib/query-builder/index.js'
+
+describe('Basic SELECT query', () => {
+  it('should generate a query with 1 field', () => {
+    const qb = new QueryBuilder()
+
+    expect(qb.select('id').from('table').toSql())
+      .toBe('SELECT id FROM table;')
+  })
+
+  it('should generate a query with 2 fields', () => {
+    const qb = new QueryBuilder()
+
+    expect(qb.select('id', 'name').from('table').toSql())
+      .toBe('SELECT id, name FROM table;')
+  })
+
+  it('should generate a SELECT * FROM', () => {
+    const qb = new QueryBuilder()
+
+    expect(qb.select().from('table').toSql()).toBe('SELECT * FROM table;')
+    expect(qb.from('table').toSql()).toBe('SELECT * FROM table;')
+  })
+})
